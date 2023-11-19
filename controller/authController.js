e41b58d304b488c2caa9f564e8ec4d9ba6d20eb6
programmerNizam <mdnizam12109@gmail.com>
2023-11-20 02:38:19 +0600

    Setup completed auth modules

---
+++ controller/authController.js
@@ -0,0 +1,16 @@
+export const register = async (req, res) => {
+    const { email, password, name, role, photo, gender } = req.body
+    try {
+        let user = null
+
+    } catch (error) {
+
+    }
+}
+export const login = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
 import mongoose from 'mongoose'
+import authRoutes from './routes/auth.js'
 
 dotenv.config()
 
@@ -32,5 +33,6 @@ const connectDB = async () => {
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors(corsOptions))
+app.use('/api/v1/auth', authRoutes)
 
 app.listen(port, () => { connectDB(), console.log('Server is running ' + port) })
