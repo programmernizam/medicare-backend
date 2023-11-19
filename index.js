6bb9a652e68441dd4f83834792f403f1b6027335
programmerNizam <mdnizam12109@gmail.com>
2023-11-20 01:38:12 +0600

    Setup middleware

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import dotenv from 'dotenv'
 import express from 'express'
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 8000
+
+const corsOptions = {
+    origin: true
+}
+
+app.get('/', (req, res) => {
+    res.send("Api is working")
+})
+
+// Middleware
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors(corsOptions))
+
+app.listen(port, () => console.log('Server is running ' + port))
