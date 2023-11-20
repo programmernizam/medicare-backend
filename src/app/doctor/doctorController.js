8c63552c5deb9dd4424ab3da03fae79228134595
programmerNizam <mdnizam12109@gmail.com>
2023-11-21 02:21:56 +0600

    Doctor delete controller created

--- src/app/doctor/doctorController.js
+++ src/app/doctor/doctorController.js
@@ -10,6 +10,15 @@ const updateDoctor = async (req, res) => {
     }
 }
 
+const deleteDoctor = async (req, res) => {
+    const id = req.params.id
+    try {
+        const deleteDoctor = await Doctor.findByIdAndDelete(id)
+        res.status(200).json({ success: true, message: 'Doctor Successfully Deleted', data: deleteDoctor })
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'Field Update' })
+    }
+}
 
 export const doctorController = {
     updateDoctor,
