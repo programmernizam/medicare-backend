257707c9b45b264f4fc81d6a2058098544d43791
programmerNizam <mdnizam12109@gmail.com>
2023-11-21 02:22:15 +0600

    Doctor single controller created

--- src/app/doctor/doctorController.js
+++ src/app/doctor/doctorController.js
@@ -19,6 +19,15 @@ const deleteDoctor = async (req, res) => {
         res.status(500).json({ success: false, message: 'Field Update' })
     }
 }
+const getSingleDoctor = async (req, res) => {
+    const id = req.params.id
+    try {
+        const findDoctor = await Doctor.findById(id).select("-password")
+        res.status(200).json({ success: true, message: 'Doctor Found Successfully', data: findDoctor })
+    } catch (error) {
+        res.status(404).json({ success: false, message: 'No user found' })
+    }
+}
 
 export const doctorController = {
     updateDoctor,
