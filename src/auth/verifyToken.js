fa835df6749521f2652aa8631c9005e3a0336593
programmerNizam <mdnizam12109@gmail.com>
2023-11-21 20:09:40 +0600

    Authentication system added

--- src/auth/verifyToken.js
+++ src/auth/verifyToken.js
@@ -1,3 +1,4 @@
+import jwt from "jsonwebtoken"
 
 export const authenticate = async (req, res, next) => {
     // Get Token From Headers
@@ -8,10 +9,18 @@ export const authenticate = async (req, res, next) => {
         return res.status(401).json({ success: false, message: 'No Token, Access Denied' })
     }
     try {
-        console.log(authToken);
+        const token = authToken.split("")[1]
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
+        req.userId = decoded.id
+        req.role = decoded.role
         next()
 
     } catch (error) {
-
+        if (error.name === 'TokenExpiredError') {
+            return res.status(401).json({
+                message: 'Token is expired'
+            })
+        }
+        return res.status(401).json({ success: false, message: 'Invalid Token' })
     }
 }
