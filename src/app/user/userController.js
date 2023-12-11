1d00884faec54bd4f944d0d9778135080411b05c
programmerNizam <mdnizam12109@gmail.com>
2023-12-11 17:38:21 +0600

    Get user appointment data api created

--- src/app/user/userController.js
+++ src/app/user/userController.js
@@ -1,3 +1,5 @@
+import Booking from '../../modules/BookingSchema.js'
+import Doctor from '../../modules/DoctorSchema.js'
 import User from "../../modules/UserSchema.js"
 const updateUser = async (req, res) => {
     const id = req.params.id
@@ -55,7 +57,27 @@ const getUserProfile = async (req, res) => {
         res.status(500).json({ success: false, message: "Something went wrong, cannot get user data" })
     }
 }
+const getMyAppointment = async (req, res) => {
+    try {
+        // Step 1: Retrieve appointments from booking for specified user
+        const bookings = await Booking.find({ user: req.userId })
 
+        // Step 2: Extract doctor ids from appointment bookings
+        const doctorIds = bookings.map(ele => ele.doctor.id)
+
+        // Step 3: Retrieve Doctors using doctor ids
+        const doctors = await Doctor.find({ _id: { $in: doctorIds } }.select('-password'))
+        res.status(200).json(
+            {
+                success: true,
+                message: 'Appointment are getting',
+                data: doctors
+            }
+        )
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Something went wrong, cannot get appointment data" })
+    }
+}
 
 export const userController = {
     updateUser,
